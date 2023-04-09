21799ec0d6065641688850530fb4eef3d9ac5850
vinibr3 <vinicius.deoliveira@outlook.com>
2023-04-09 09:07:07 -0300

    Add admin environment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   scope '/:locale' do
     resources :sessions, only: %i[new create destroy]
     resources :registrations, only: %i[new create]
